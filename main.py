b2f4a0fdd953a5d7162496e0d9abd586035c44ee
Patrick Conboy <rispir45@gmail.com>
2018-09-19 01:55:33 -0400

    Copied over assignment1.py twitter code and added documentation

--- main.py
+++ main.py
@@ -1,2 +1,70 @@
-print('hello')
-jkjkj
+# Project for CS328 - Data Management/Data Wrangling
+# Coded by: Od Ganzorig and Patrick Conboy
+# Date Created: 9/18/2018
+# Project Description: Web API that provides user access to data analytics on 
+# tweets containing mentions of Overwatch and heroes in the game.
+
+#!/usr/bin/python3
+
+# Accessing the Twitter API
+# This script describes the basic methodology for accessing a Twitter feed
+# or something similar.
+
+# Loading libraries needed for authentication and requests
+import operator
+from requests_oauthlib import OAuth2Session
+from oauthlib.oauth2 import BackendApplicationClient
+import json
+
+# In order to use this script, you must:
+# - Have a Twitter account and create an app
+# - Store in keys.json a property called "twitter" whose value is an
+#     object with two keys, "key" and "secret"
+with open('keys.json', 'r') as f:
+   keys = json.loads(f.read())['twitter']
+
+twitter_key = keys['key']
+twitter_secret = keys['secret']
+
+# We authenticate ourselves with the above credentials
+# We will demystify this process later
+#
+# For documentation, see http://requests-oauthlib.readthedocs.io/en/latest/api.html
+# and http://docs.python-requests.org/en/master/
+client = BackendApplicationClient(client_id=twitter_key)
+oauth = OAuth2Session(client=client)
+token = oauth.fetch_token(token_url='https://api.twitter.com/oauth2/token',
+                          client_id=twitter_key,
+                          client_secret=twitter_secret)
+
+# Base url needed for all subsequent queries
+base_url = 'https://api.twitter.com/1.1/'
+
+# Particular page requested. The specific query string will be
+# appended to that.
+page = 'search/tweets.json'
+
+# Depending on the query we are interested in, we append the necessary string
+# As you read through the twitter API, you'll find more possibilities
+req_url = base_url + page + '?q=Hanover+College&tweet_mode=extended&count=100'
+
+# We perform a request. Contains standard HTTP information
+response = oauth.get(req_url)
+
+# Read the query results
+results = json.loads(response.content.decode('utf-8'))
+
+## Process the results
+## CAUTION: The following code will attempt to read up to 10000 tweets that
+## Mention Hanover College. You should NOT change this code.
+tweets = results['statuses']
+while True:
+   if not ('next_results' in results['search_metadata']):
+      break
+   if len(tweets) > 10000:
+      break
+   next_search = base_url + page + results['search_metadata']['next_results'] + '&tweet_mode=extended'
+   print(results['search_metadata']['next_results'])
+   response = oauth.get(next_search)
+   results = json.loads(response.content.decode('utf-8'))
+   tweets.extend(results['statuses'])
