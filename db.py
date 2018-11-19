3e96ef9c25ec989ce232319cd608be7d866e8b50
Patrick Conboy <rispir45@gmail.com>
2018-11-19 01:59:57 -0500

    Deleted unnecessary documentation and code from db.py

--- db.py
+++ db.py
@@ -4,12 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from json import dumps
 
-# Loading libraries needed for authentication and requests
-import operator
-import json
-from requests_oauthlib import OAuth2Session
-from oauthlib.oauth2 import BackendApplicationClient
-
 Base = declarative_base()
 
 # Class for Races. Each race has a unique id, name, and faction
@@ -56,69 +50,4 @@ class Db:
       newRace = Race(name=name, id=id, faction=faction, description=description)
       self.session.add(newRace)
       return newRace
-
-############## END OF CLASS AND TABLE DEFINITIONS ##
-
-
-
-############## BEGIN BUILDING DATABASE #############
-
-# # Create engine for our database
-# engine = create_engine('sqlite:///:memory:', echo=True)
-
-# # Drop existing tables and recreate them
-# Base.metadata.drop_all(engine)
-# Base.metadata.create_all(engine)
-
-# # Create a session for us to use while adding things to the database
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# newRace = Race()
-
-# ############## FINISH BUILDING DATABASE ############
-
-
-
-# ############## BEGIN WEB REQUESTS ##################
-
-# # In order to use this script, you must:
-# # - Have a Blizzard API account and create an app in order to obtain a clientID and secret
-# # - Store in keys.json a property called "blizzardAPI" whose value is an
-# #     object with two keys, "clientID" and "secret"
-# with open('keys.json', 'r') as f:
-#    keys = json.loads(f.read())['blizzardAPI']
-
-# blizzardAPI_clientID = keys['clientID']
-# blizzardAPI_secret = keys['secret']
-
-# # We authenticate ourselves with the above credentials
-
-# # For documentation, see http://requests-oauthlib.readthedocs.io/en/latest/api.html
-# # and http://docs.python-requests.org/en/master/
-# client = BackendApplicationClient(client_id=blizzardAPI_clientID)
-# oauth = OAuth2Session(client=client)
-# token = oauth.fetch_token(token_url='https://US.battle.net/oauth/token',
-#                           client_id=blizzardAPI_clientID,
-#                           client_secret=blizzardAPI_secret)
-
-# # Base url needed for all subsequent queries
-# base_url = 'https://us.api.blizzard.com/wow/'
-
-# # Particular page requested. The specific query string will be
-# # appended to that.
-# page = 'data/character/races?locale=en_US&access_token=USTncSSBoKRvJ1PGU6la6d6syvxeNyrtqV'
-
-# # This just combines our base_url and page into our request_url
-# req_url = base_url + page
-
-# # We perform a request. Contains standard HTTP information
-# response = oauth.get(req_url)
-
-# # Read the query results
-# results = json.loads(response.content.decode('utf-8'))
-
-# for race in results['races']:
-#   print(race['name'])
-
-
+      
